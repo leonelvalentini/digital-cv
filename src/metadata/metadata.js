7a2b0fd3df14a96bf0161022861dff7d7589adbc
Leonel Valentini <>
2022-06-08 20:09:00 -0300

    updated project

---
+++ src/metadata/metadata.js
@@ -0,0 +1,89 @@
+module.exports = {
+  name: 'Leonel Valentini',
+  title: 'Director de tecnología',
+  facts: {
+    'Ubicación': 'Buenos Aires, Argentina',
+    'Email': '<a href="mailto:leonelvalentini@gmail.com">leonelvalentini@gmail.com</a>'
+  },
+  skills: [
+    'Team Leadership',
+    'Decision-Making',
+    'Scrum',
+    'Agile Methodologies',
+    'Cloud Computing',
+    'Amazon Web Services (AWS)',
+    'Mobile Applications',
+    'Web Applications',
+    'Full-Stack developer',
+    'Java',
+    'Javascript',
+    'Typescript',
+    'HTML5',
+    'CCS3',
+    'C#',
+    'Swift',
+    'PHP',
+    'NodeJS',
+    'MySQL',
+    'NoSQL',
+    'MongoDB',
+    'MSSQL',
+    'Angular',
+    'Git'
+  ],
+  experience: [
+    {
+      title: 'Amanda Care - Director de tecnología',
+      written: 'Enero 2019 - Presente',
+      badges: [],
+      contents: `Amanda Care es la primer asistente virtual de habla hispana en usar inteligencia artificial para monitorear pacientes.
+      Mejora la adherencia al tratamiento y previene la prevalencia de factores de riesgo.
+      Para los proveedores de atención médica y las aseguradoras que desean reducir los costos relacionados con la readmisión de pacientes con enfermedades crónicas,
+      Amanda Care mejorará la adherencia al tratamiento.
+      A diferencia de otras aplicaciones y centros de llamadas, Amanda Care usa Whatsapp e IA para involucrar a pacientes y médicos.`
+    },
+    {
+      title: 'Colmena Farms - Director de tecnología',
+      written: 'Enero 2018 - Enero 2019',
+      badges: [],
+      contents: `Colmena es una plataforma digital enfocada en conectar a las personas en el campo, facilitándoles compartir y obtener información con sus equipos y empresas.
+      Cuenta con 20.000 usuarios en más de 6 países.`
+    },
+    {
+      title: 'iPhoideas / IPH Digital - Gerente de tecnología',
+      written: 'Mayo 2009 - Enero 2018',
+      badges: [],
+      contents: ``
+    },
+    {
+      title: 'Despegar.com - Desarrollador de software',
+      written: 'Mayo 2005 - Mayo 2009',
+      badges: [],
+      contents: ``
+    }
+  ],
+  publications: [
+    {
+      title: 'La fascinación de crear para celulares inteligentes',
+      written: 'Diciembre 2012',
+      badges: [],
+      name: 'cronista.com',
+      link: `https://www.cronista.com/pyme/La-fascinacion-de-crearpara-celulares-inteligentes-20121227-0023.html`
+    },
+    {
+      title: 'SAP Argentina incorporó a iPhoideas como socio del programa “SAP Mobile Apps Partner Center”',
+      written: 'Octubre 2012',
+      badges: [],
+      name: 'todoenunclick.com',
+      link: `https://www.todoenunclick.com/sap-argentina-incorporo-a-iphoideas-como-socio-del-programa-sap-mobile-apps-partner-center/`
+    },
+    {
+      title: 'La “iManía” tiene su pata en la Argentina',
+      written: 'Abril 2010',
+      badges: [],
+      name: 'diariouno.com.ar',
+      link: `https://www.diariouno.com.ar/edimpresa/2010/04/26/nota241713.html`
+    }
+  ]
+
+};
