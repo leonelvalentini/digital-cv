eded7fdb85eab33ac8197e409ca9d789e6ceb329
Leonel Valentini <>
2022-06-09 16:35:40 -0300

    updated cv

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -40,26 +40,73 @@ module.exports = {
       Mejora la adherencia al tratamiento y previene la prevalencia de factores de riesgo.
       Para los proveedores de atención médica y las aseguradoras que desean reducir los costos relacionados con la readmisión de pacientes con enfermedades crónicas,
       Amanda Care mejorará la adherencia al tratamiento.
-      A diferencia de otras aplicaciones y centros de llamadas, Amanda Care usa Whatsapp e IA para involucrar a pacientes y médicos.`
+      A diferencia de otras aplicaciones y centros de llamadas, Amanda Care usa Whatsapp e IA para involucrar a pacientes y médicos.`,
+      items: [
+        'Gestión de equipos evolutivos',
+        'Modularización de sistemas',
+        'Integración de Dialogflow para AI',
+        'Solución para Covid-19 con atención a mas de 150mil paciente',
+        'Versión Alpha',
+        'Generación de MVP de Amanda Care'
+      ]
     },
     {
       title: 'Colmena Farms - Director de tecnología',
       written: 'Enero 2018 - Enero 2019',
       badges: [],
       contents: `Colmena es una plataforma digital enfocada en conectar a las personas en el campo, facilitándoles compartir y obtener información con sus equipos y empresas.
-      Cuenta con 20.000 usuarios en más de 6 países.`
+      Cuenta con 20.000 usuarios en más de 6 países.`,
+      items: [
+        'Gestión de equipos: Web Java y Android'
+      ]
     },
     {
       title: 'iPhoideas / IPH Digital - Gerente de tecnología',
       written: 'Mayo 2009 - Enero 2018',
       badges: [],
-      contents: ``
+      contents: `Fundada en el año 2008 para satisfacer la demanda creciente de desarrollo de aplicaciones para las plataformas móbiles. 
+      Integrado por un equipo de Management con una extensa trayectoria, en conjunto con expertos en publicidad, marketing, diseño y desarrollo.`,
+      items: [
+        'Ascenso a CTO',
+        'Gerenciamento de equipos',
+        'Generación de sistemas web',
+        'Creación de equipo iOS',
+        'Creación de equipo Android',
+        'Creación de equipo Blackberry',
+        'Sincro',
+        'BA Subte',
+        'Infobae',
+        'Televisa',
+        'Amigos 2012',
+        'Descorchados',
+        'Me Encanta',
+        'ESPN SCRUM',
+        'Tequila Azul',
+        'Lecciones de Tango Argentino',
+        'Eat Healthy',
+        'ESPN 107.9 FM',
+        'Masaryk.tv',
+        'Desarrollo de primeras aplicaciones en iOS para latinoamerica',
+        'Autocapacitación en desarrollo Mobile de iOS y Symbian'
+      ]
     },
     {
       title: 'Despegar.com - Desarrollador de software',
       written: 'Mayo 2005 - Mayo 2009',
       badges: [],
-      contents: ``
+      contents: `Portal en Internet más amigable para la compra y consulta de vuelos, hoteles y transporte.
+      Permite reservar viajes en tiempo real entre más de 500 aerolíneas, 80000 hoteles y 50 agencias de alquiler de vehículos y miles de paquetes turísticos en todo el mundo.`,
+      items: [
+        'Módulo de armado de paquetes turísticos',
+        'Optimización de sistema Core en búsqueda de pasajes',
+        'Actualización de módulo para venta de pasajes en .NET',
+        'Generación de módulo para reserva de hoteles',
+        'Gestión de equipo de nuevas funcionalidades de backend',
+        'Gestión de equipo de mantenimiento',
+        'Caso de exito en ventas debido a un workflow nuevo adaptado a la usabilidad',
+        'Actualización de modulo de reservas por un modulo de compra real',
+        'Mantiniendo del sistema realizado en ASP'
+      ]
     }
   ],
   publications: [
